67acecaac8d3fb2a52aacc34b3610a9e01bac12f
SakshamVashisht1 <sakshamvashisht01@gmail.com>
2025-07-08 15:13:32 +0530

    Lecture6

---
+++ L-6/script.js
@@ -0,0 +1,66 @@
+const promise1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('PROMISE RESOLVED')
+    }, 3000)
+})
+
+const promise2 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        reject('PROMISE REJECTED')
+    }, 3000)
+})
+
+function somethingSomething(message, delay) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(message)
+            resolve('PROMISE RESOLVED', message)
+        }, delay)
+    })
+}
+function somethingSomethingRej(message, delay) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log(message)
+            reject('PROMISE REJECTED', message)
+        }, delay)
+    })
+}
+
+// promise1
+//     .then((res) => console.log(res))
+//     .catch((err) => console.log('ERROR', err))
+//     .then(() => {
+//         return somethingSomething('P2', 4000)
+//     })
+//     .catch((err) => console.log('ERROR', err))
+//     .then(() => {
+//         return somethingSomethingRej('P3', 3000)
+//     })
+//     .catch((err) => console.log('ERROR', err))
+//     .then(() => {
+//         return somethingSomething('P4', 1000)
+//     })
+//     .catch((err) => console.log('ERROR', err))
+
+promise2
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err))
+    .then(() =>
+        setTimeout(() => {
+            console.log('P2')
+        }, 2000)
+    )
+    .catch((err) => console.log(err))
+    .then(() =>
+        setTimeout(() => {
+            console.log('P3')
+        }, 4000)
+    )
+    .catch((err) => console.log(err))
+    .then(() =>
+        setTimeout(() => {
+            console.log('P4')
+        }, 3000)
+    )
+    .catch((err) => console.log(err))
